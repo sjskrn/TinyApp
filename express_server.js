4b14f32eb9d99c7990fc923dff1c5a307d021ef1
Jaskaran Singh <s.jaskaran4@gmail.com>
2018-02-05 13:01:11 +0000

    pushing all changes

--- express_server.js
+++ express_server.js
@@ -1,70 +1,111 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const bcrypt = require("bcrypt")
+const cookieParser = require('cookie-parser');
+const app = express();
 
-function generateRandomString() {
-  const alphaNumeric = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  let randString = "";
-  for (let i = 0; i < 6; i++) {
-    randString += randString.charAt(Math.floor(Math.random() * randString.length));
-  }
+app.use(cookieParser());
 
-  return randString;
-
-}
-
-app.set("view engine", "ejs");
-
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-  "rachet": "http://www.canadiantire.ca/en/pdp/stanley-1-4-in-black-chrome-ratchet-0582002p.html#srp"
-};
+const PORT = process.env.PORT || 8080;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
-});
+app.set("view engine", "ejs"); 
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp listening on port ${PORT}!, visit localhost:8080 in your browser`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+function generateRandomString() {
+  let randomString = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++)
+    randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+  return randomString;
+}
+
+app.get("/", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"],
+    urlDatabase: urlDatabase
+  };
+  res.render("main", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+  res.clearCookie('password')
+  res.redirect("/");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"],
+    urlDatabase: urlDatabase
+  };
+  res.render("urls_index", templateVars); 
+});
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"],
+    shortUrl: req.params.id,
+    longUrl: urlDatabase[req.params.id]
+  };
+  res.render("urls_new", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"],
+    shortUrl: req.params.id,
+    longUrl: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  console.log("req.params:", req.params);
+  console.log("urlDatabase[req.params.id] to be deleted:", urlDatabase[req.params.id]);
+  delete urlDatabase[req.params.id];
+  console.log("urlDatabase:", urlDatabase);
+  res.redirect("/urls");
+});
+
+app.post("/urls", (req, res) => {
+  console.log("Post parameters", req.body);
+  console.log("Long URL is ", req.body.longUrl);
+  let shortUrl = generateRandomString();
+  console.log("Short URL is ", shortUrl);
+  urlDatabase[shortUrl] = req.body.longUrl;
+  console.log("updated URL Database?", urlDatabase);
+  res.redirect(`/urls/${shortUrl}`);
+});
+
+app.post("/urls/:id", (req, res) => {
+  console.log("changing the long URL for this req.params:", req.params);
+  console.log("urlDatabase[req.params.id] to be changed:", urlDatabase[req.params.id]);
+  console.log("updated longUrl:", req.body.longUrl);
+  urlDatabase[req.params.id] = req.body.longUrl;
+  console.log("urlDatabase:", urlDatabase);
+  res.redirect("/urls");
+});
+
+app.get("/u/:shortUrl", (req, res) => {
+  const longUrl = urlDatabase[req.params.shortUrl];
+  res.redirect(longUrl);
 });
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
-  res.render("urls_show", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-
-app.post("/urls/:id/delete", (req, res) =>{
-delete urlDatabase[req.params.id]
- res.redirect("/urls");
 });
